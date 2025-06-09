6f64d03d1d7b9cf30b211952ae21eff9378a87b0
Cleiton S. Guilhermite <161074166+Draky-Rollgard@users.noreply.github.com>
2025-06-09 16:40:03 -0400

    Modulo06
    JASON

---
+++ aulaJSON_conversao_JS_J.js
@@ -0,0 +1,4 @@
+let usuario = { nome: "Maria", idade: 28 };
+let jsonConvertido = JSON.stringify(usuario);
+
+console.log(jsonConvertido); // '{"nome":"Maria","idade":28}'
---
+++ aulaJSON_conversao_J_JS.js
@@ -0,0 +1,4 @@
+let jsonString = '{"nome": "João", "idade": 30}';
+let objeto = JSON.parse(jsonString);
+
+console.log(objeto.nome); // João
