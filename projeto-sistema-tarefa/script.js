32bbf99bcf900ed213266e2f7b84660962a9e99b
Cleiton S. Guilhermite <161074166+Draky-Rollgard@users.noreply.github.com>
2025-06-10 18:32:56 -0400

    Design-e-Lista

---
+++ projeto-sistema-tarefa/script.js
@@ -0,0 +1,28 @@
+document.getElementById("form-tarefa").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const input = document.getElementById("input-tarefa");
+    const texto = input.value.trim();
+
+    if(texto == "") return;
+
+    adicionarTarefa(texto);
+    input.value = "";
+});
+function adicionarTarefa(texto) {
+  const lista = document.getElementById("lista-tarefas");
+
+  const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.textContent = texto;
+
+  const botaoRemover = document.createElement("button");
+  botaoRemover.textContent = "Remover";
+  botaoRemover.onclick = () => li.remove();
+
+  li.appendChild(span);
+  li.appendChild(botaoRemover);
+
+  lista.appendChild(li);
+}
